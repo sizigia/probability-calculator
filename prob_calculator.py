4024b10b14e713225c9c6bc381ec950ea0627526
Faustina Maria <faustina.giaquinta@gmail.com>
2020-10-14 12:46:05 -0400

    committing changes -  experiment function not passing tests yet

--- prob_calculator.py
+++ prob_calculator.py
@@ -10,6 +10,30 @@ class Hat:
             for key, value in kwargs.items():
                 self.contents.extend([key] * value)
 
+    def draw(self, amount):
+        draws = []
+        for _ in range(amount):
+            if self.contents:
+                item = random.choice(self.contents)
+                draws.append(item)
+                self.contents.remove(item)
+            else:
+                return draws
+
+        return draws
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    m = 0
+    colors = expected_balls.keys()
+
+    for _ in range(num_experiments):
+        if hat.contents:
+            draw_ = hat.draw(num_balls_drawn)
+            actual_balls_drawn = [draw_.count(color) for color in colors]
+
+            if (actual_balls_drawn >=
+                    list(expected_balls.values())):
+                m += 1
+
+    return m / num_experiments
